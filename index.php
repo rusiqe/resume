71e6ecbc00b2e66c310df88dc2d31af137f3d85d
rusiqe <rusiqe@gmail.com>
2013-09-17 09:20:13 +0200

    Create index.php
    first edit

---
+++ index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html> 
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" id="viewport"
+content="width=device-width, height=device-height,
+initial-scale=1.0, maximum-scale=1.0,
+user-scalable=no;" />
+<title>Tau</title>
+<link href="jquery-mobile/jquery.mobile.theme-1.0.min.css" rel="stylesheet" type="text/css"/>
+<link href="jquery-mobile/jquery.mobile.structure-1.0.min.css" rel="stylesheet" type="text/css"/>
+<style type="text/css">
+.hiddentext {
+	color: #CCC;
+}
+</style>
+<script src="jquery-mobile/jquery-1.6.4.min.js" type="text/javascript"></script>
+<script src="jquery-mobile/jquery.mobile-1.0.min.js" type="text/javascript"></script>
+</head> 
+<body> 
+
+<div data-role="page" id="page">
+	<div data-role="header">
+	  <h1>Welcome to Taurai Resume App</h1>
+	</div>
+	<div data-role="content">	
+		<ul data-role="listview">
+			<li><a href="#page2">Personal Details</a></li>
+            <li><a href="#page3">Qualifications</a></li>
+			<li><a href="#page4">Contact me!</a></li>
+		</ul>		
+	</div>
+	<div data-role="footer">
+		<h4>tau.resume</h4>
+	</div>
+</div>
+
+<div data-role="page" id="page2">
+	<div data-role="header">
+		<h1>Personal Profile</h1>
+	</div>
+	<div data-role="content">
+      <table width="460">
+        <tr>
+          <td width="215"><strong>DATE OF BIRTH:</strong></td>
+          <td width="222">23 -APRIL -1989</td>
+        </tr>
+        <tr>
+          <td><strong>SEX:</strong></td>
+          <td>MALE</td>
+        </tr>
+        <tr>
+          <td><strong>HEIGHT:</strong></td>
+          <td>~183CM [6 ft 0 inch]</td>
+        </tr>
+        <tr>
+          <td><strong>BUILT:</strong></td>
+          <td>ATHELETIC</td>
+        </tr>
+        <tr>
+          <td><strong>AGE:</strong></td>
+          <td>24</td>
+        </tr>
+        <tr>
+          <td><strong>RELIGION:</strong></td>
+          <td><span class="center">CHRISTIAN (CATHOLIC)</span></td>
+        </tr>
+      </table>
+      <p>_______________<img src="images/pic1.JPG" alt="potrait" width="217" height="248" class="center">_______________</p>
+	</div>
+	<div data-role="footer">
+		<h4>return</h4>
+	</div>
+</div>
+
+<div data-role="page" id="page3">
+	<div data-role="header">
+		<h1>Qualifications</h1>
+	</div>
+	<div data-role="content">
+	  <table width="464">
+	    <tr>
+	      <td width="245"><strong>‘O’ LEVEL:</strong></td>
+	      <td width="207">(2002-2005)</td>
+        </tr>
+	    <tr>
+	      <td colspan="2">SANYATI BAPTIST HIGH SCHOOL </td>
+        </tr>
+	    <tr>
+	      <td><strong>PASSES:</strong> 6 As, 4Bs, 1C.</td>
+	      <td>&nbsp;</td>
+        </tr>
+	    <tr>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+        </tr>
+	    <tr>
+	      <td><strong>‘A’ LEVEL:</strong></td>
+	      <td> (2006-2007)</td>
+        </tr>
+	    <tr>
+	      <td>JAMESON HIGH SCHOOL (2007)</td>
+	      <td>&nbsp;</td>
+        </tr>
+	    <tr>
+	      <td colspan="2"><strong>PASSES:</strong> MATHEMATICS{C}, PHYSICS<br>
+          {D}, CHEMISTRY {E}</td>
+        </tr>
+	    <tr>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+        </tr>
+	    <tr>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+        </tr>
+	    <tr>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+        </tr>
+	    <tr>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+        </tr>
+	    <tr>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+        </tr>
+	    <tr>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+        </tr>
+	    <tr>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+        </tr>
+	    <tr>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+        </tr>
+	    <tr>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+        </tr>
+	    <tr>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+        </tr>
+	    <tr>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+        </tr>
+	    <tr>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+        </tr>
+	    <tr>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+        </tr>
+      </table>
+	</div>
+	<div data-role="footer">
+		<h4>return</h4>
+	</div>
+</div>
+
+<div data-role="page" id="page4">
+	<div data-role="header">
+		<h1> Contact Details</h1>
+	</div>
+	<div data-role="content">	
+		Content		
+	</div>
+	<div data-role="footer">
+		<h4>return</h4>
+	</div>
+</div>
+
+</body>
+</html>
